a84178ce9b90cf1ad1052bb42c0c99134c937269
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-11-24 00:11:39 +0100

    Configure cors and json

--- netlify/edge-functions/site-title.ts
+++ netlify/edge-functions/site-title.ts
@@ -1,3 +1,13 @@
-export default () => new Response("Hello world");
+export default () => {
+  const res = {
+    value: "hello world"
+  }
+  return new Response(JSON.stringify(res), {
+  status: 200,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  }
+});}
 
 export const config = { path: "/site-title" };
