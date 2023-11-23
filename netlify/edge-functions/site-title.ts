56bf59e5dc4590358b29c26f86ffc48a6e4e3d3d
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-11-24 00:55:25 +0100

    Improve error handling and fetch site

--- netlify/edge-functions/site-title.ts
+++ netlify/edge-functions/site-title.ts
@@ -7,17 +7,42 @@ const jsonResponse = <T extends {}>(json: T, statusCode = 200) =>
     },
   });
 
+class HttpError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode = 400) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
 export default async (request: Request) => {
   const inboundUrl = new URL(request.url);
-  const outboundUrl = inboundUrl.searchParams.get("url");
-  if (outboundUrl === null) {
-    const res = { error: "missing request parameter: url" };
-    return jsonResponse(res, 400);
-  }
+  try {
+    const rawOutboundUrl = inboundUrl.searchParams.get("url");
+    if (rawOutboundUrl === null) {
+      throw new HttpError("missing request parameter: url");
+    }
 
-  return jsonResponse({
-    value: `hello world -- ${outboundUrl}`,
-  });
+    if (!URL.canParse(rawOutboundUrl)) {
+      throw new HttpError(`url is not valid: ${rawOutboundUrl}`);
+    }
+
+    const siteRes = await fetch(rawOutboundUrl);
+    const siteHtml = await siteRes.text();
+    console.log(siteRes);
+    console.log(siteHtml);
+
+    return jsonResponse({
+      value: `hello world -- ${rawOutboundUrl}`,
+    });
+  } catch (e) {
+    if (e instanceof HttpError) {
+      return jsonResponse({ error: e.message }, e.statusCode);
+    } else {
+      console.error(inboundUrl, e);
+      return jsonResponse({ error: "internal server error" }, 500);
+    }
+  }
 };
 
 export const config = { path: "/site-title" };
